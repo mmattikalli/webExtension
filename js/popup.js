15dfd70329e36d5ee02492749d2c8a8d90a9ad9d
Mattikalli Megha <t-mematt@microsoft.com>
2018-07-13 14:07:58 -0700

    Extension Updates

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,21 @@
 let red = document.getElementById('changeToRed');
 let blue = document.getElementById('changeToBlue');
 let reset = document.getElementById('reset');
+let IDinfo = document.getElementById('faceID'); 
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("mySwitch");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+} 
 
 // use tabs.insertCSS to change header color on button click
 // red
