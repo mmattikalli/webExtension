4e4d4e32ceae317222aa498deca53938ce341478
Michael Duan <t-miduan@microsoft.com>
2018-07-30 10:27:49 -0700

    dfghjkl

--- js/faceID.js
+++ js/faceID.js
@@ -107,3 +107,20 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 700);
     }
 });
+
+const handleMessage = (request, sender, sendResponse) => {
+    console.log("Message from the content script: " +
+        request.greeting);
+    sendResponse({ response: "Response from background script" });
+};
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+
+
+
+
+
+
+
+
