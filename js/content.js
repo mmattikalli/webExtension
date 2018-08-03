24bda2651ff61501e356f6607687c6e1affbded5
mmattikalli <40609941+mmattikalli@users.noreply.github.com>
2018-08-03 15:33:43 -0700

    addBlur and removeBlur functions added (#33)
    * addBlur and removeBlur functions added
    
    * more changes

--- js/content.js
+++ js/content.js
@@ -1,4 +1,5 @@
 var counter = 0;
+let newWebsiteDiv = document.createElement("div");
 
 //Listener that appends a video element with webcam stream as src
 browser.runtime.onMessage.addListener(
@@ -8,75 +9,6 @@ browser.runtime.onMessage.addListener(
                 "from a content script:" + sender.tab.url :
                 "from the extension");
 
-            // move all of website html into a div
-            let newWebsiteDiv = document.createElement("div"); 
-            //websiteBody.className = "websiteBody"; 
-            let websiteBody = document.body.innerHTML; 
-            if (websiteBody != null)
-            {
-                console.log("something"); 
-            }
-            document.body.innerHTML = ""; 
-            document.body.appendChild(newWebsiteDiv); 
-            newWebsiteDiv.innerHTML = websiteBody; 
-            newWebsiteDiv.className = "newWebsiteDiv"; 
-           
-            // sets document zindex lower than the divContainer     
-            document.body.style.zIndex = "10000";
-
-            // creates video element 
-            let video = document.createElement("video");
-
-            //creates new div for video 
-            let divContainer = document.createElement("div");
-            divContainer.id = "divContainer";
-
-            // clears preset div settings 
-            divContainer.style.clear = "both";
-
-            // sets divContainer to be a flexbox
-            divContainer.style.display = "flex";
-
-            // formatting elements within the divContainer
-            divContainer.style.alignItems = "center";
-            divContainer.style.justifyContent = "center";
-            divContainer.style.flexDirection = "column";
-
-            divContainer.style.position = "fixed";
-            divContainer.style.zIndex = "100000000";
-            divContainer.style.width = "100%";
-            divContainer.style.height = "100%";
-
-            divContainer.style.top = "35%";
-            divContainer.style.left = "50%";
-            divContainer.style.right = "50%";
-            divContainer.style.bottom = "50%";
-            divContainer.style.transform = "translate(-50%, -50%)";
-
-            // setting up video element
-            video.autoplay = true;
-            video.id = "vid";
-            video.style.width = "450px";
-            video.style.height = "450px";
-            video.style.borderRadius = "350px";
-
-            // adds video to divContainer 
-            divContainer.appendChild(video);
-
-            // creates text to put on screen with video element 
-            let para = document.createElement("h1");
-            para.id = "para";
-            para.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
-            para.style.fontSize = "40px"; 
-            para.style.textAlign = "center"; 
-            para.innerHTML = "Calibrating...";
-            divContainer.appendChild(para);
-
-            // video attached to divContainer, attached to webpage
-            document.body.appendChild(divContainer);
-
-            // blurs only the background text 
-            newWebsiteDiv.style.filter = "blur(20px)";
 
             //Getting the video element, first checking if the user has an accessible webcam
             if (navigator.mediaDevices.getUserMedia) {
@@ -104,3 +36,80 @@ browser.runtime.onMessage.addListener(
 
     }
 );
+/**
+ * Creates a new div to move current webpage html into
+ * Creates a new div to append video and text
+ * Blurs the current webpage 
+ */
+function addBlur() {
+    // move all of website html into a div
+    
+    let websiteBody = document.body.innerHTML;
+    document.body.innerHTML = "";
+
+    document.body.appendChild(newWebsiteDiv);
+    newWebsiteDiv.innerHTML = websiteBody;
+
+    // sets document zindex lower than the divContainer     
+    document.body.style.zIndex = "10000";
+
+    // creates video element 
+    let video = document.createElement("video");
+
+    //creates new div for video 
+    let divContainer = document.createElement("div");
+    divContainer.id = "divContainer";
+
+    // clears preset div settings 
+    divContainer.style.clear = "both";
+
+    // sets divContainer to be a flexbox
+    divContainer.style.display = "flex";
+
+    // formatting elements within the divContainer
+    divContainer.style.alignItems = "center";
+    divContainer.style.justifyContent = "center";
+    divContainer.style.flexDirection = "column";
+
+    divContainer.style.position = "fixed";
+    divContainer.style.zIndex = "100000000";
+    divContainer.style.width = "100%";
+    divContainer.style.height = "100%";
+
+    divContainer.style.top = "35%";
+    divContainer.style.left = "50%";
+    divContainer.style.right = "50%";
+    divContainer.style.bottom = "50%";
+    divContainer.style.transform = "translate(-50%, -50%)";
+
+    // setting up video element
+    video.autoplay = true;
+    video.id = "vid";
+    video.style.width = "450px";
+    video.style.height = "450px";
+    video.style.borderRadius = "350px";
+    video.style.zIndex = "21434536743436566";
+
+    // adds video to divContainer 
+    divContainer.appendChild(video);
+
+    // creates text to put on screen with video element 
+    let para = document.createElement("h1");
+    para.id = "para";
+    para.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+    para.style.fontSize = "40px";
+    para.style.textAlign = "center";
+    para.innerHTML = "Calibrating...";
+    divContainer.appendChild(para);
+
+    // video attached to divContainer, attached to webpage
+    document.body.appendChild(divContainer);
+
+    // blurs only the background text 
+    newWebsiteDiv.style.filter = "blur(20px)";
+}
+
+function removeBlur() {
+    newWebsiteDiv.style.filter = "none"; 
+    document.body.innerHTML = newWebsiteDiv.innerHTML;
+}
