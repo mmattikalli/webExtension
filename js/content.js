1e0762433c61f4854e9d4726a2696c10b71e2535
Mattikalli Megha <t-mematt@microsoft.com>
2018-08-03 11:17:13 -0700

    video stream + blurring working

--- js/content.js
+++ js/content.js
@@ -8,8 +8,21 @@ browser.runtime.onMessage.addListener(
                 "from a content script:" + sender.tab.url :
                 "from the extension");
 
+            // move all of website html into a div
+            let newWebsiteDiv = document.createElement("div"); 
+            //websiteBody.className = "websiteBody"; 
+            let websiteBody = document.body.innerHTML; 
+            if (websiteBody != null)
+            {
+                console.log("something"); 
+            }
+            document.body.innerHTML = ""; 
+            document.body.appendChild(newWebsiteDiv); 
+            newWebsiteDiv.innerHTML = websiteBody; 
+            newWebsiteDiv.className = "newWebsiteDiv"; 
+           
+            // sets document zindex lower than the divContainer     
             document.body.style.zIndex = "10000";
-            //document.body.style.filter = "blur(20px)";
 
             // creates video element 
             let video = document.createElement("video"); 
@@ -24,7 +37,7 @@ browser.runtime.onMessage.addListener(
             // sets divContainer to be a flexbox
             divContainer.style.display = "flex";
 
-            // moving elements within the divContainer using flexbox properties 
+            // formatting elements within the divContainer
             divContainer.style.alignItems = "center";
             divContainer.style.justifyContent = "center";
             divContainer.style.flexDirection = "column";
@@ -40,30 +53,29 @@ browser.runtime.onMessage.addListener(
             divContainer.style.bottom = "50%";
             divContainer.style.transform = "translate(-50%, -50%)";
 
-            //Setting up video element
+            // setting up video element
             video.autoplay = true;
             video.id = "vid";
             video.style.width = "450px";
             video.style.height = "450px";
-            // video.style.position = "fixed";
-            // video.style.top = "30%"; 
-            // video.style.left = "50%"; 
-            // video.style.transform = "translate(-50%, -50%)";
             video.style.borderRadius = "350px";
 
+            // adds video to divContainer 
             divContainer.appendChild(video);
 
+            // creates text to put on screen with video element 
             let para = document.createElement("h1");
             para.style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
             para.style.fontSize = "40px"; 
             para.style.textAlign = "center"; 
-            para.innerHTML = "WebAssist FaceID Technology Calibrating...";
+            para.innerHTML = "Calibrating...";
             divContainer.appendChild(para);
 
+            // video attached to divContainer, attached to webpage
             document.body.appendChild(divContainer);
 
-
-            //divContainer.style.filter = "none";
+            // blurs only the background text 
+            newWebsiteDiv.style.filter = "blur(20px)";
 
             //Getting the video element, first checking if the user has an accessible webcam
             if (navigator.mediaDevices.getUserMedia) {
@@ -85,10 +97,3 @@ browser.runtime.onMessage.addListener(
         }
     }
 );
-
-let isBlurred = false;
-
-// content script recieves message from background to perform an action 
-if (window.location.href == "https://www.wikipedia.org/") {
-    document.body.style.filter = "blur(20px)";
-}
