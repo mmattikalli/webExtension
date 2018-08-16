f3c7d53ca2fdc2ae732dac54a404ad69fdb4eef1
Michael Duan <t-miduan@microsoft.com>
2018-08-16 11:45:31 -0700

    Better slouch detection

--- js/slouchdetection.js
+++ js/slouchdetection.js
@@ -5,7 +5,8 @@
 class SlouchDetectEventHandler extends CameraControllerEventHandler {
     onFrame(frame, tab, faces) {
         if (faces.length > 0) {
-            if (faces[0].faceRectangle !== null && (faces[0].faceRectangle.height > 250 || faces[0].faceRectangle.width > 250)) {
+            console.log(JSON.stringify(faces));
+            if (slouchDetect(faces)) {
                 browser.tabs.sendMessage(tab, { type: 'AlertSlouch' });
             }
         }
@@ -36,3 +37,26 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
         }
     }
 });
+
+function slouchDetect(faces) {
+    let calibratedFaceCenter = {
+        x: m_CameraController.calibrateInfo.face.faceRectangle.left + ((m_CameraController.calibrateInfo.face.faceRectangle.width) / 2),
+        y: m_CameraController.calibrateInfo.face.faceRectangle.top + ((m_CameraController.calibrateInfo.face.faceRectangle.height) / 2)
+    }
+
+    let faceCenter = { //Get center of face in frame
+        x: faces[0].faceRectangle.left + ((faces[0].faceRectangle.width) / 2),
+        y: faces[0].faceRectangle.top + ((faces[0].faceRectangle.height) / 2),
+    }
+
+    if (faces[0].faceRectangle !== null) {
+        if (faces[0].faceRectangle.height > 250 || faces[0].faceRectangle.width > 250) {
+            console.log("big face");
+            return true;
+        } else if (faceCenter.y - calibratedFaceCenter.y > 30) {
+            console.log("bad back");
+            return true;
+        }
+    }
+    return false;
+}
